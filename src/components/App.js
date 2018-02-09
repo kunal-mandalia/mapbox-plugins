d8e7b2e4e7740a18707c399a67d6160e0495f10d
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-02-09 00:58:50 +0000

    feat: integrate react-mapbox-gl

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Map from './Map.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
